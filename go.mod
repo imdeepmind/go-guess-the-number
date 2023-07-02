5d87ed7ea3f5601c557acdd316faf2ed618d1cac
Abhishek Chatterjee <abhishek@imdeepmind.com>
2023-07-03 00:33:47 +0530

    added some comments in the code

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module guess-the-number
 
 go 1.20
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,34 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/fatih/color"
+	"github.com/inancgumus/screen"
 )
 
 func main() {
+	// Clear the screen
+	screen.Clear()
+	screen.MoveTopLeft()
+
+	// Making a guess
 	var target int = rand.Intn(100)
 	var guess int
 	var totalGuesses int = 0
 
-	fmt.Println("Welcome to Guess The Number!!!")
+	// Opening messages
+	color.Red("Welcome to Guess The Number!!!")
+	color.Cyan("Please guess the number between 0 and 100")
 
 	for true {
-		fmt.Print("Please guess a number: ")
+		// Asking for input from the screen
+		fmt.Print("Your guess: ")
 		fmt.Scanln(&guess)
 
+		// Printing the typed number
 		fmt.Printf("You guessed %v\n", guess)
 
+		// Checking the guess with target
 		if guess == target {
-			fmt.Printf("Congratulations, you won the game with %v guesses\n", totalGuesses)
+			color.Green("Congratulations, you won the game with %v guesses\n", totalGuesses)
 			return
 		}
 
+		// Proving direction to the user based on guessed number and target
 		if guess > target {
 			fmt.Println("The target is smaller")
 		} else {
 			fmt.Println("The target is bigger")
 		}
 
+		// Maintaining a counter for counting guesses
 		totalGuesses++
-
 	}
 	fmt.Println("Please guess a number: ")
 }
